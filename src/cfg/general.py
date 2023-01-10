dbcdc127811af2282679a4339b3b64b6d5c39791
kanazashi-s <kanazashi-s@aidemy.co.jp>
2023-01-10 11:06:09 +0900

    fixed config

--- src/cfg/general.py
+++ src/cfg/general.py
@@ -6,10 +6,10 @@ class GeneralCFG:
     is_kaggle = bool(int(os.environ["IS_KAGGLE_ENVIRONMENT"]))
     data_dir = Path("/kaggle", "input", "rsna-breast-cancer-detection") if is_kaggle else Path("/workspace", "data")
     raw_data_dir = Path("/kaggle", "input", "rsna-breast-cancer-detection") if is_kaggle else data_dir / "raw"
-    processed_data_dir = data_dir / "processed"
+    processed_data_dir = Path("/kaggle", "working", "processed") if is_kaggle else data_dir / "processed"
     png_data_dir = data_dir / "png_converted"
     train_image_dir = png_data_dir / "theo_512"
-    test_image_dir = png_data_dir / "test_png_512"
+    test_image_dir = Path("/kaggle", "working", "test_png_512") if is_kaggle else png_data_dir / "test_png_512"
     image_size = 512
     data_version = "vanilla"
     debug = False
@@ -22,7 +22,6 @@ class GeneralCFG:
 
 
 if GeneralCFG.is_kaggle:
-    GeneralCFG.test_image_dir = Path("/kaggle", "working", "test_png_512")
     GeneralCFG.j2k_dir = Path("/kaggle", "working", "j2k_dir")
 
 
