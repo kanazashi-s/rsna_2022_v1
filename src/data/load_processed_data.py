652dec9a9ba10ef2dee773a5b4e2f5ca9188e1de
kanazashi-s <kanazashi-s@aidemy.co.jp>
2023-01-10 11:25:19 +0900

    fixed config

--- src/data/load_processed_data.py
+++ src/data/load_processed_data.py
@@ -4,20 +4,20 @@ from cfg.general import GeneralCFG
 
 
 def train(seed: int):
-    input_path = Path("/workspace", "data", "processed", GeneralCFG.data_version, f"seed{seed}")
+    input_path = GeneralCFG.processed_data_dir / GeneralCFG.data_version / f"seed{seed}"
     return pd.read_csv(input_path / "train.csv")
 
 
 def test(seed: int):
-    input_path = Path("/workspace", "data", "processed", GeneralCFG.data_version, f"seed{seed}")
+    input_path = GeneralCFG.processed_data_dir / GeneralCFG.data_version / f"seed{seed}"
     return pd.read_csv(input_path / "test.csv")
 
 
 def sample_submission(seed: int):
-    input_path = Path("/workspace", "data", "processed", GeneralCFG.data_version, f"seed{seed}")
+    input_path = GeneralCFG.processed_data_dir / GeneralCFG.data_version / f"seed{seed}"
     return pd.read_csv(input_path / "sample_submission.csv")
 
 
 def sample_oof(seed: int):
-    input_path = Path("/workspace", "data", "processed", GeneralCFG.data_version, f"seed{seed}")
+    input_path = GeneralCFG.processed_data_dir / GeneralCFG.data_version / f"seed{seed}"
     return pd.read_csv(input_path / "sample_oof.csv")
