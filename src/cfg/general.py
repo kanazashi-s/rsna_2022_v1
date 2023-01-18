c4d273bd1120b617659635e234383b29aaa41fbe
kanazashi-s <kanazashi-s@aidemy.co.jp>
2023-01-18 11:44:41 +0900

    fixed config

--- src/cfg/general.py
+++ src/cfg/general.py
@@ -8,8 +8,8 @@ class GeneralCFG:
     raw_data_dir = Path("/kaggle", "input", "rsna-breast-cancer-detection") if is_kaggle else data_dir / "raw"
     processed_data_dir = Path("/kaggle", "working", "processed") if is_kaggle else data_dir / "processed"
     png_data_dir = data_dir / "png_converted"
-    train_image_dir = png_data_dir / "test_png_512"
-    test_image_dir = Path("/kaggle", "working", "test_png_512") if is_kaggle else png_data_dir / "test_png_512"
+    train_image_dir = png_data_dir / "test_png_1024"
+    test_image_dir = Path("/kaggle", "working", "test_png_1024") if is_kaggle else png_data_dir / "test_png_1024"
     image_size = 512
     data_version = "vanilla"
     debug = False
