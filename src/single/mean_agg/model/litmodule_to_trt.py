64fcb2aed93a829af5b6f6d957b3ce044548a158
kanazashi-s <kanazashi-s@aidemy.co.jp>
2023-02-15 09:19:41 +0900

    modified tensorrt convert script

--- src/single/mean_agg/model/litmodule_to_trt.py
+++ src/single/mean_agg/model/litmodule_to_trt.py
@@ -1,6 +1,7 @@
 import sys
-from cfg.general import GeneralCFG
-if GeneralCFG.is_kaggle:
+import os
+is_kaggle = bool(int(os.environ["IS_KAGGLE_ENVIRONMENT"]))
+if is_kaggle:
     sys.path.append('/kaggle/input/timm-0-6-9/pytorch-image-models-master')
 
 from pathlib import Path
@@ -9,9 +10,6 @@ import torch.nn as nn
 import pytorch_lightning as pl
 import torch_tensorrt
 import timm
-from single.mean_agg.config import MeanAggCFG
-from utils.model_utils.focal_loss import SigmoidFocalLoss
-from metrics import get_scores
 
 
 class MyModel(nn.Module):
@@ -19,59 +17,29 @@ class MyModel(nn.Module):
         super().__init__()
 
         # define pretrained model
-        self.is_next_vit = MeanAggCFG.model_name.startswith("nextvit")
-        if self.is_next_vit:
-            model_config = {
-                "model_name": MeanAggCFG.model_name,
-                "num_classes": 1000,
-                "drop": MeanAggCFG.drop_rate,
-                # "path_dropout": MeanAggCFG.drop_path_rate,
-                "pretrained": False,
-            }
-            self.backbone = timm.create_model(**model_config)
-            large_checkpoint = torch.load("pretrained_weights/nextvit_base_in1k6m_384.pth")
-            self.backbone.load_state_dict(large_checkpoint['model'])
-            self.backbone.stem[0].conv.in_channels = 1
-            self.backbone.stem[0].conv.weight.data = self.backbone.stem[0].conv.weight.data[:, 0:1, :, :]
-            self.backbone = nn.Sequential(*list(self.backbone.children())[:-1])
+        model_config = {
+            "model_name": "efficientnetv2_rw_s",
+            "num_classes": 0,  # to use feature extractor,
+            "in_chans": 1,
+            "drop_rate": 0.2,
+            "drop_path_rate": 0.2,
+        }
+        if is_kaggle:
+            self.backbone = timm.create_model(**model_config, pretrained=False)
         else:
-            model_config = {
-                "model_name": MeanAggCFG.model_name,
-                "num_classes": 0,  # to use feature extractor,
-                "in_chans": 1,
-                "drop_rate": MeanAggCFG.drop_rate,
-                "drop_path_rate": MeanAggCFG.drop_path_rate,
-            }
-            if GeneralCFG.is_kaggle:
-                self.backbone = timm.create_model(**model_config, pretrained=False)
-            else:
-                self.backbone = timm.create_model(**model_config, pretrained=True)
+            self.backbone = timm.create_model(**model_config, pretrained=True)
 
-        self.fc = nn.Linear(1024 if self.is_next_vit else self.backbone.num_features, 1)
-        # self._init_weights(self.fc)
-        self.loss = self._get_loss()
-        self.learning_rate = MeanAggCFG.lr
+        self.fc = nn.Linear(self.backbone.num_features, 1)
 
     def forward(self, inputs):
         features = self.backbone(inputs)
         output = self.fc(features)
         return output
 
-    @staticmethod
-    def _get_loss():
-        if MeanAggCFG.loss_function == "BCEWithLogitsLoss":
-            return nn.BCEWithLogitsLoss(pos_weight=torch.tensor(MeanAggCFG.pos_weight))
-        elif MeanAggCFG.loss_function == "SigmoidFocalLoss":
-            return SigmoidFocalLoss(
-                gamma=MeanAggCFG.focal_loss_gamma,
-                alpha=MeanAggCFG.focal_loss_alpha,
-                sigmoid=True
-            )
-
 
 def litmodule_to_trt(model_dir: Path, seed: int):
     model_dir = model_dir / f"seed{seed}"
-    for fold in GeneralCFG.train_fold:
+    for fold in range(5):
         # lightning のモデルを、 checkpoint から読み込み、 torch のモデルに変換
         state_dict = torch.load(model_dir / f"best_loss_fold{fold}.ckpt")["state_dict"]
         model_torch = MyModel()
@@ -87,18 +55,18 @@ def litmodule_to_trt(model_dir: Path, seed: int):
                 dtype=torch.half,
             )
         ]
-        enabled_precisions = {torch.float, torch.half}  # Run with fp16
+        enabled_precisions = {torch.half}  # Run with fp16
 
         trt_ts_module = torch_tensorrt.compile(
             model_torch, inputs=inputs, enabled_precisions=enabled_precisions
         )
 
-        save_dir = Path.cwd() if GeneralCFG.is_kaggle else model_dir
+        save_dir = Path.cwd() if is_kaggle else model_dir
         torch.jit.save(trt_ts_module, save_dir / f"trt_fold{fold}.ts")
 
 
 if __name__ == "__main__":
-    if GeneralCFG.is_kaggle:
+    if is_kaggle:
         model_dir = Path("/kaggle/input/mean-agg-swa-20230213")
         litmodule_to_trt(model_dir, seed=42)
     else:
