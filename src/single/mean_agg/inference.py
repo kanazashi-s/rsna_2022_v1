4de8ba8e3672b527f20a93b16a55a2ca231ceff6
kanazashi-s <kanazashi-s@aidemy.co.jp>
2023-02-16 14:33:49 +0900

    modified tensorrt convert script

--- src/single/mean_agg/inference.py
+++ src/single/mean_agg/inference.py
@@ -2,13 +2,15 @@ from pathlib import Path
 import numpy as np
 import polars as pol
 import torch
+from torch.utils.data import DataLoader
 import torch_tensorrt
 import pytorch_lightning as pl
 from tqdm import tqdm
 from cfg.general import GeneralCFG
 from single.mean_agg.config import MeanAggCFG
-from single.mean_agg.data_module import DataModule
 from data import load_processed_data_pol
+from single.mean_agg.dataset import TestDataset
+from single.mean_agg.transforms import get_transforms
 from single.mean_agg.model.lit_module import LitModel
 from single.mean_agg.model.litmodule_to_trt import MyModel
 from metrics import calc_oof_score
@@ -19,31 +21,34 @@ def inference(seed):
     submission_df = load_processed_data_pol.sample_submission(seed=seed)
     test_df = load_processed_data_pol.test(seed=seed).with_column(
         pol.lit(0).alias("prediction")
+    ).with_column(
+        (pol.col("patient_id").cast(pol.Utf8) + "_" + pol.col("image_id").cast(pol.Utf8) + ".png")
+        .alias("image_filename")
     )
 
-    predictions_list = []
     for fold in GeneralCFG.train_fold:
         input_dir = MeanAggCFG.trt_model_dir / f"seed{seed}"
         trt_ts_module = torch.jit.load(input_dir / f"trt_fold{fold}.ts")
 
-        data_module = DataModule(
-            seed=seed,
-            fold=fold,
-            batch_size=MeanAggCFG.batch_size,
-            num_workers=GeneralCFG.num_workers,
+        transforms_no_aug = get_transforms(augment=False)
+        test_dataset = TestDataset(test_df, transforms_no_aug, is_inference=True)
+        test_dataloader = DataLoader(
+            test_dataset,
+            batch_size=GeneralCFG.batch_size,
+            shuffle=False,
+            num_workers=GeneralCFG.num_workers
         )
-        data_module.setup()
 
         # torch のモデルを使って、 test_df の prediction を予測
         fold_preds = []
-        for batch in tqdm(data_module.test_dataloader()):
+        for batch in test_dataloader:
             with torch.no_grad():
                 image = batch
                 image = image.cuda().half()
                 fold_preds.append(trt_ts_module(image))
 
         fold_preds = (torch.concat(fold_preds, axis=0).cpu().detach().float().numpy())
-        fold_preds = 1 / (1 + np.exp(-fold_preds))
+        fold_preds = 1 / (1 + torch.exp(-fold_preds))
 
         # test_df の prediction に、 fold_preds を fold 数で割って足す
         test_df = test_df.with_column(
@@ -73,7 +78,12 @@ def inference(seed):
     return predictions_df
 
 
-def calc_seed_mean(seeds=GeneralCFG.seeds):
+def calc_seed_mean(seeds=GeneralCFG.seeds, num_workers=None, batch_size=None):
+    if num_workers is not None:
+        GeneralCFG.num_workers = num_workers
+    if batch_size is not None:
+        GeneralCFG.batch_size = batch_size
+
     predictions_df_list = []
     for seed in seeds:
         predictions = inference(seed)
